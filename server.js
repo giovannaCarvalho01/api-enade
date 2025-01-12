453ac85e297ed71ee77edf9af8d2d430eed6edd4
giovannaCarvalho01 <giovannavital45@gmail.com>
2025-01-12 14:08:04 -0300

    feat: new endpoint

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-import { createClient } from '@supabase/supabase-js';
 import cors from 'cors';
 import express from 'express';
 import dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
+import ExcelJS from 'exceljs';
 
 dotenv.config();
 
 const app = express();
 const PORT = 3001;
 
-// Configuração do Supabase
-const supabaseUrl = 'https://yonxzkpeawyzeytbnslt.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
-
+// Configuração do middleware
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
@@ -20,171 +17,307 @@ app.use(cors({
 }));
 app.use(express.json());
 
-// API ANO
-app.get('/api/ano', async (req, res) => {
+// Configuração da conexão com o banco de dados
+const db = await mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 255,
+  queueLimit: 0,
+});
+
+// Rota GET Ano
+app.get('/anos', async (req, res) => {
   try {
-    const { data, error } = await supabase
-      .from('dim_ano')
-      .select('ano');
-
-    if (error) throw error;
-
-    res.json(data);
+    const [rows] = await db.query('SELECT DISTINCT ano FROM vw_curso_completo'); // Substitua 'sua_tabela' pelo nome da sua tabela
+    res.json(rows);
   } catch (error) {
-    console.error('Erro ao buscar ano:', error);
-    res.status(500).json({ error: 'Erro ao buscar ano no banco de dados.' });
+    console.error(error);
+    res.status(500).send('Erro ao buscar dados do banco de dados');
   }
 });
 
-// API IES
-// http://localhost:3001/api/ies/2022
-app.get('/api/ies/:ano', async (req, res) => {
-  const { ano } = req.params;
-
-  if (!ano) {
-    return res.status(400).json({ error: 'O parâmetro "ano" é obrigatório.' });
-  }
-
-  try {
-    // Consultar a view que já tem o filtro para ano
-    const { data, error } = await supabase
-      .from('vw_ies_ano') // Consultando a view
-      .select('cod_ies')
-      .eq('ano', ano) // Filtrando pela coluna 'ano'
-      .limit(10); // Limite de resultados se necessário
-
-    if (error) throw error;
-
-    res.json(data); // Retorna os dados da view
-  } catch (error) {
-    console.error('Erro ao buscar ies:', error);
-    res.status(500).json({ error: 'Erro ao buscar ies no banco de dados.' });
-  }
-});
-
-// API CURSO
-// http://localhost:3001/api/curso/2022/1
-app.get('/api/curso/:year/:ies', async (req, res) => {
-  const { year, ies } = req.params;
-
-  if (!year || !ies) {
-    return res.status(400).json({ error: 'Os parâmetros "ano" e "ies" são obrigatórios.' });
-  }
-
-  try {
-    // Consultar a view que já tem o filtro para ano e cod_ies
-    const { data, error } = await supabase
-      .from('vw_curso_ano_ies') // Consultando a view
-      .select('cod_curso')
-      .eq('ano', year) // Filtrando pelo ano
-      .eq('cod_ies', ies) // Filtrando pela instituição de ensino
-      .limit(10); // Limite de resultados
-
-    if (error) throw error;
-
-    res.json(data); // Retorna os cursos
-  } catch (error) {
-    console.error('Erro ao buscar cursos:', error);
-    res.status(500).json({ error: 'Erro ao buscar cursos no banco de dados.' });
-  }
-});
-
-
-// API CONDIÇÃO DE SALA
-// http://localhost:3001/api/cond_sala/2022/65435
-app.get('/api/cond_sala/:year/:curso', async (req, res) => {
-  const { year, curso } = req.params;
-
-  if (!year || !curso) {
-    return res.status(400).json({ error: 'Os parâmetros "year" e "curso" são obrigatórios.' });
-  }
-
-  try {
-    // Consultar a view que já tem os filtros para ano, curso e tipo de presença
-    const { data, error } = await supabase
-      .from('vw_cond_sala') // Consultando a view
-      .select()
-      .eq('ano', year) // Filtrando pelo ano
-      .eq('cod_curso', curso) // Filtrando pelo código do curso
-      .limit(10); // Limite de resultados
-
-    if (error) throw error;
-
-    res.json(data); // Retorna a condição de sala
-  } catch (error) {
-    console.error('Erro ao buscar condição do curso:', error);
-    res.status(500).json({ error: 'Erro ao buscar condição do curso no banco de dados.' });
-  }
-});
-
-
-// API BOX PLOT - VERIFICAR CURSO 100966
-// http://localhost:3001/api/boxplot/2022/117004
-app.get('/api/boxplot/:ano/:curso', async (req, res) => {
-  const { ano, curso } = req.params;
-
-  try {
-    // Consultar a view que já tem a junção e os filtros para o tipo de presença '555'
-    const { data, error } = await supabase
-      .from('vw_cond_sala') // Consultando a view
-      .select('nota')
-      .eq('ano', ano) // Filtrando pelo ano
-      .eq('cod_curso', curso); // Filtrando pelo código do curso
+// Rota GET Regiao com parâmetro 'ano'
+app.get('/regiao/:ano', async (req, res) => {
+    const ano = req.params.ano; // Obtém o valor do parâmetro 'ano' na URL
     
-    if (error) throw error;
+    try {
+      // Consulta SQL com parâmetro 'ano' na cláusula WHERE
+      const [rows] = await db.query('SELECT DISTINCT dsc_regiao_completo FROM vw_curso_completo WHERE ano = ?', [ano]);
+      
+      res.json(rows);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Erro ao buscar dados do banco de dados');
+    }
+});
+  
 
-    const notas = data.map(row => row.nota);
+// Rota GET UF com parâmetro 'ano' e 'regiao'
+app.get('/uf/:ano/:regiao', async (req, res) => {
+    const ano = req.params.ano; // Obtém o valor do parâmetro 'ano' na URL
+    const regiao = req.params.regiao; // Obtém o valor do parâmetro 'ano' na URL
     
-    console.log('Notas:', notas);  // Verificar as notas obtidas
+    try {
+      // Consulta SQL com parâmetro 'ano' na cláusula WHERE
+      const [rows] = await db.query('SELECT DISTINCT dsc_uf FROM vw_curso_completo WHERE ano = ? AND dsc_regiao_completo = ?', [ano, regiao]);
+      
+      res.json(rows);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Erro ao buscar dados do banco de dados');
+    }
+});
 
-    // Ordenar as notas para calcular os quartis
-    const sortedNotas = [...notas].sort((a, b) => a - b);
+// Rota GET Municipio com parâmetro 'ano' e 'regiao' e 'uf'
+app.get('/municipio/:ano/:regiao/:uf', async (req, res) => {
+    const ano = req.params.ano; // Obtém o valor do parâmetro 'ano' na URL
+    const regiao = req.params.regiao; // Obtém o valor do parâmetro 'regiao' na URL
+    const uf = req.params.uf; // Obtém o valor do parâmetro 'uf' na URL
+    
+    try {
+      // Consulta SQL com parâmetro 'ano' na cláusula WHERE
+      const [rows] = await db.query('SELECT DISTINCT dsc_municipio FROM vw_curso_completo WHERE ano = ? AND dsc_regiao_completo = ? AND dsc_uf = ?', [ano, regiao, uf]);
+      
+      res.json(rows);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Erro ao buscar dados do banco de dados');
+    }
+});
 
-    const calculatePercentile = (arr, percentile) => {
-      const index = (percentile / 100) * (arr.length - 1);
-      if (Number.isInteger(index)) {
-        return arr[index];
+// Rota GET Categoria administrativa com parâmetro 'ano' e 'regiao' e 'uf'
+app.get('/cat_adm/:ano/:regiao/:uf/:municipio', async (req, res) => {
+    const ano = req.params.ano; // Obtém o valor do parâmetro 'ano' na URL
+    const regiao = req.params.regiao; // Obtém o valor do parâmetro 'regiao' na URL
+    const uf = req.params.uf; // Obtém o valor do parâmetro 'uf' na URL
+    const municipio = req.params.municipio; // Obtém o valor do parâmetro 'uf' na URL
+    
+    try {
+      // Consulta SQL com parâmetro 'ano' na cláusula WHERE
+      const [rows] = await db.query('SELECT DISTINCT dsc_cat_adm FROM vw_curso_completo WHERE ano = ? AND dsc_regiao_completo = ? AND dsc_uf = ? AND dsc_municipio = ?', [ano, regiao, uf, municipio]);
+      
+      res.json(rows);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Erro ao buscar dados do banco de dados');
+    }
+});
+
+// Rota GET IES administrativa com parâmetro 'ano' e 'regiao' e 'uf', 'MUNICIPIO' e cattegoria adm
+app.get('/ies/:ano/:regiao/:uf/:municipio/:cat_adm', async (req, res) => {
+    const ano = req.params.ano; // Obtém o valor do parâmetro 'ano' na URL
+    const regiao = req.params.regiao; // Obtém o valor do parâmetro 'regiao' na URL
+    const uf = req.params.uf; // Obtém o valor do parâmetro 'uf' na URL
+    const municipio = req.params.municipio; // Obtém o valor do parâmetro 'uf' na URL
+    const cat_adm = req.params.cat_adm; // Obtém o valor do parâmetro 'uf' na URL
+    
+    try {
+      // Consulta SQL com parâmetro 'ano' na cláusula WHERE
+      const [rows] = await db.query('SELECT DISTINCT cod_ies FROM vw_curso_completo WHERE ano = ? AND dsc_regiao_completo = ? AND dsc_uf = ? AND dsc_municipio = ? AND dsc_cat_adm = ?', [ano, regiao, uf, municipio, cat_adm]);
+      
+      res.json(rows);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Erro ao buscar dados do banco de dados');
+    }
+});
+
+// Rota GET Grupo com parâmetro 'ano' e 'regiao' e 'uf', 'municipio' e 'categoria adm'
+app.get('/grupo/:ano/:regiao/:uf/:municipio/:cat_adm/:cod_ies', async (req, res) => {
+    const ano = req.params.ano; // Obtém o valor do parâmetro 'ano' na URL
+    const regiao = req.params.regiao; // Obtém o valor do parâmetro 'regiao' na URL
+    const uf = req.params.uf; // Obtém o valor do parâmetro 'uf' na URL
+    const municipio = req.params.municipio; // Obtém o valor do parâmetro 'uf' na URL
+    const cat_adm = req.params.cat_adm; // Obtém o valor do parâmetro 'uf' na URL
+    const cod_ies = req.params.cod_ies; // Obtém o valor do parâmetro 'uf' na URL
+    
+    try {
+      // Consulta SQL com parâmetro 'ano' na cláusula WHERE
+      const [rows] = await db.query('SELECT DISTINCT dsc_grp FROM vw_curso_completo WHERE ano = ? AND dsc_regiao_completo = ? AND dsc_uf = ? AND dsc_municipio = ? AND dsc_cat_adm = ? AND cod_ies = ?', [ano, regiao, uf, municipio, cat_adm, cod_ies]);
+      
+      res.json(rows);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Erro ao buscar dados do banco de dados');
+    }
+});
+
+
+// FILTER
+
+app.get('/filter', async (req, res) => {
+    // Desestruturando os parâmetros da query string
+    const { cod_ies, cat_adm, municipio, regiao, uf, grp, ano, coluna } = req.query;
+  
+    // Definindo as colunas permitidas para evitar injeção de SQL
+    const colunasPermitidas = [
+      'dsc_regiao_completo',
+      'dsc_cat_adm',
+      'dsc_municipio',
+      'dsc_uf',
+      'dsc_grp',
+      'cod_ies',
+      'ano'
+    ];
+  
+    // Validando se a coluna passada é uma das colunas permitidas
+    const colunaSelecionada = colunasPermitidas.includes(coluna) ? coluna : 'dsc_regiao_completo';
+  
+    // Inicia a consulta SQL base
+    let query = `SELECT DISTINCT ${colunaSelecionada} FROM vw_curso_completo WHERE 1=1`;  // Usa a coluna selecionada
+  
+    // Array para armazenar os parâmetros da consulta
+    const params = [];
+  
+    // Adiciona as condições para os outros filtros
+    if (cod_ies) {
+      query += ' AND cod_ies = ?';
+      params.push(cod_ies);
+    }
+  
+    if (cat_adm) {
+      query += ' AND dsc_cat_adm = ?';
+      params.push(cat_adm);
+    }
+  
+    if (municipio) {
+      query += ' AND dsc_municipio = ?';
+      params.push(municipio);
+    }
+  
+    if (regiao) {
+      query += ' AND dsc_regiao_completo = ?';
+      params.push(regiao);
+    }
+  
+    if (uf) {
+      query += ' AND dsc_uf = ?';
+      params.push(uf);
+    }
+  
+    if (grp) {
+      query += ' AND dsc_grp = ?';
+      params.push(grp);
+    }
+  
+    if (ano) {
+      query += ' AND ano = ?';
+      params.push(ano);
+    }
+  
+    try {
+      // Executa a consulta com os parâmetros dinâmicos
+      const [rows] = await db.query(query, params);
+      res.json(rows);  // Retorna os resultados filtrados
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Erro ao buscar dados do banco de dados');
+    }
+  });
+
+//   baixar notas
+app.get('/notas/download', async (req, res) => {
+    // Pegando os parâmetros de filtro da query string
+    const { cod_ies, cat_adm, municipio, regiao, uf, grp, ano, presenca } = req.query;
+  
+    // Inicia a consulta SQL
+    let query = 'SELECT * FROM vw_curso_notas WHERE 1=1';
+    const params = [];
+  
+    // Adicionando filtros à consulta
+    if (cod_ies) {
+      query += ' AND cod_ies = ?';
+      params.push(cod_ies);
+    }
+  
+    if (cat_adm) {
+      query += ' AND dsc_cat_adm = ?';
+      params.push(cat_adm);
+    }
+  
+    if (municipio) {
+      query += ' AND dsc_municipio = ?';
+      params.push(municipio);
+    }
+  
+    if (regiao) {
+      query += ' AND dsc_regiao_completo = ?';
+      params.push(regiao);
+    }
+  
+    if (uf) {
+      query += ' AND dsc_uf = ?';
+      params.push(uf);
+    }
+  
+    if (grp) {
+      query += ' AND dsc_grp = ?';
+      params.push(grp);
+    }
+  
+    if (ano) {
+      query += ' AND ano = ?';
+      params.push(ano);
+    }
+  
+    // Filtro para tipo_presenca, se fornecido
+    if (presenca) {
+      query += ' AND tipo_presenca = ?';
+      params.push(presenca);
+    }
+  
+    try {
+      // Executa a consulta SQL para pegar os dados
+      const [rows] = await db.query(query, params);
+  
+      // Cria um novo workbook e uma worksheet
+      const workbook = new ExcelJS.Workbook();
+      const worksheet = workbook.addWorksheet('Notas');
+  
+      // Adiciona as colunas no arquivo Excel
+      if (rows.length > 0) {
+        const colunas = Object.keys(rows[0]);
+        worksheet.columns = colunas.map(coluna => ({ header: coluna, key: coluna }));
+  
+        // Adiciona os dados na worksheet
+        rows.forEach(row => {
+          worksheet.addRow(row);
+        });
+  
+        // Define o nome do arquivo Excel
+        const nomeArquivo = `notas_${Date.now()}.xlsx`;
+  
+        // Define os headers para download do arquivo
+        res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        res.setHeader('Content-Disposition', `attachment; filename=${nomeArquivo}`);
+  
+        // Envia o arquivo Excel para o cliente
+        await workbook.xlsx.write(res);
+        res.end();
       } else {
-        const lower = arr[Math.floor(index)];
-        const upper = arr[Math.ceil(index)];
-        return (lower + upper) / 2;
+        res.status(404).send('Nenhum dado encontrado para os filtros fornecidos');
       }
-    };
-
-    const Q1 = calculatePercentile(sortedNotas, 25);
-    const Q3 = calculatePercentile(sortedNotas, 75);
-    const median = calculatePercentile(sortedNotas, 50);  // Mediana
-    const IQR = Q3 - Q1;
-
-    // Ajustar o cálculo de fences
-    const lowerFence = Q1 - 1.5 * IQR;
-    const upperFence = Q3 + 1.5 * IQR;
-
-    // Calcular os limites ajustados para o boxplot
-    const adjustedMin = sortedNotas.find(nota => nota >= lowerFence) || sortedNotas[0];
-    const adjustedMax = sortedNotas.reverse().find(nota => nota <= upperFence) || sortedNotas[sortedNotas.length - 1];
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Erro ao gerar o arquivo Excel');
+    }
+  });
     
-    // Filtrar os outliers e garantir que sejam únicos
-    const outliers = Array.from(new Set(notas.filter(nota => nota < lowerFence || nota > upperFence)));
+// app.post('/dados', async (req, res) => {
+//   const { coluna1, coluna2 } = req.body; // Substitua pelas colunas do seu banco
+//   try {
+//     const result = await db.query('INSERT INTO sua_tabela (coluna1, coluna2) VALUES (?, ?)', [coluna1, coluna2]);
+//     res.status(201).send({ id: result.insertId });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send('Erro ao inserir dados no banco de dados');
+//   }
+// });
 
-    console.log('Outliers:', outliers); // Verificar quais notas são identificadas como outliers
-
-    res.json({
-      data: {
-        box: [adjustedMin, Q1, median, Q3, adjustedMax],  // Retorna os limites ajustados
-        outliers: outliers,                              // Retorna os outliers
-      },
-    });
-  } catch (error) {
-    console.error('Erro ao buscar dados para boxplot:', error);
-    res.status(500).json({ error: 'Erro ao buscar os dados para boxplot.' });
-  }
-});
-
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
+// Inicia o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
-
-export default app;
