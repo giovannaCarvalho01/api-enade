815200e1ccb485f7b39f68efdc2236ed45d9ac7d
giovannaCarvalho01 <giovannavital45@gmail.com>
2025-01-30 19:43:38 -0300

    feat: ajuste porta

--- api/index.js
+++ api/index.js
@@ -23,6 +23,7 @@ const db = await mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 255,
   queueLimit: 0,
